f44097fc9fcf5de9e227e8ab5d795118359fcac1
徐赞 (Frederique Hsu) <frederique.hsu@outlook.com>
2022-09-24 22:25:56 +0800

    Illustrate the Composite Builder pattern, and demonstrate how to design such a builder pattern

---
+++ Creational_Patterns/Builder/Composite_Builder/person_builder.cpp
@@ -0,0 +1,12 @@
+/*!
+ *  \file   person_builder.cpp
+ *  \brief  Implement the concrete class PersonBuilder
+ *
+ */
+
+#include "person_builder.hpp"
+
+PersonBuilder::PersonBuilder(const std::string& person_name)
+    : PersonBuilderBase(p), p(person_name)
+{
+}
